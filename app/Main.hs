c2ed96c2c385a149ca267f5f88ecbfee1973c800
AHMAD GHOZI FIDINILLAH <ahmad.ghozi01@ui.ac.id>
2023-11-11 17:02:55 +0700

    Update code to create a simple window with a text

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Maze Game" (1366, 768) (277,156)
+
+background :: Color
+background = white
+
+title :: Picture 
+title = translate (-300) (-100) (text "Maze Game")
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window background title
